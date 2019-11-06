dd7beea792a5574a6d4f76c11f4e1cbdd6de4b8f
문상혁 <msk2292@naver.com>
2019-11-07 00:41:06 +0900

    first commit

---
+++ producer/src/main/java/producer/kafka/kr/KafkaConfig.java
@@ -0,0 +1,9 @@
+package producer.kafka.kr;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaConfig {
+
+}
